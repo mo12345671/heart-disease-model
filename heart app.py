452b7450b60edd6c305b6a7ec1a4a54d1af3deff
mo12345671 <moamedaleh@gmail.com>
2025-08-12 19:48:24 +0100

    heart app.py

--- heart app.py
+++ heart app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import pickle
+from sklearn.preprocessing import MinMaxScaler
 import plotly.graph_objects as go
 import io
 import base64
