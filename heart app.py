9267f723226069fe7db71c3dd7cb942bccba919f
mo12345671 <moamedaleh@gmail.com>
2025-08-12 21:16:07 +0100

    Update heart app.py

--- heart app.py
+++ heart app.py
@@ -6,7 +6,7 @@ import pickle
 def load_model():
     with open("heart_disease.sav", "rb") as f:
         return pickle.load(f)
-selected_features = X.columns[selector.get_support()].tolist()
+selected_features = x.columns[selector.get_support()].tolist()
 @st.cache_data
 def load_selected_features():
     with open("selected_features.pkl", "rb") as f:
@@ -33,3 +33,4 @@ if st.button("Predict"):
             st.success(f"✅ Low risk of heart disease. Probability: {proba:.2f}")
     except Exception as e:
         st.error(f"Prediction error: {e}")
+
